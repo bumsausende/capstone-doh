bebf1ebb37914141319d261a7484d77c449ff0b5
bumsausende <79257621+bumsausende@users.noreply.github.com>
2021-11-10 10:39:28 +0100

    name change

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -4,36 +4,10 @@ const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Lekton&display=swap');
  html,
 body {
-background-color: #5DC8A8;
+  background-color: #5DC8A8;
   padding: 0;
   margin: 0;
   font-family: "Lekton";}
-
-a {
-  color: inherit;
-  text-decoration: none;
-}
-
-* {
-  box-sizing: border-box;
-}
-h1 {
-    font-size: 2rem;
-    text-align: center;
-    text-transform: uppercase;
-  }
-  img {
-    border-radius: 5px;
-    height: auto;
-    width: 10rem;
-  }
-  div {
-    text-align: center;
-  }
-  small {
-    display: block;
-  }
-  a {
-    color: ${({ theme }) => theme.primaryHover};
-    text-decoration: none;}
 `;
+
+
