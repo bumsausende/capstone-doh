2dc9a6ceb86936e4784a40821ec5d9ea781dae00
bumsausende <79257621+bumsausende@users.noreply.github.com>
2021-11-11 11:49:27 +0100

    add toDOH/ test array.map

--- pages/toDOH.jsx
+++ pages/toDOH.jsx
@@ -9,11 +9,39 @@ import {
   import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
   import NextLink from "next/link";
 
+  const toDOHs =[
+    {
+        id:1,
+        name: "Male das Pony",
+        condition: "drinnen",
+        description1: "Hol dir ein Blatt Papier und einen Stift und male ein Pony, das eine Möhre Kaut und einen Hut auf hat",
+        description2: "Setze deinen Handy-Timer auf 15 Minuten"
+    },
+    {
+        id:2,
+        name: "Putze dein Küchenfenster",
+        condition: "drinnen",
+        description1: "Schnapp dir einen Lappen, Glasspiritus und einen Abzieher und gönn dir den Putz",
+        description2: "Setze deinen Handy-Timer auf 15 Minuten"
+    },
+    {
+        id:3,
+        name: "Hüpf nach draußen und bringe drei kleine Blumen wiedeer mit",
+        condition: "draußen",
+        description1: "Hol dir ein Blatt Papier und einen Stift und male ein Pony, das eine Möhre Kaut und einen Hut auf hat",
+        description2: "Setze deinen Handy-Timer auf 15 Minuten und pack die armen Blumen in die Vase!"
+    }
+];
   
-  const toDOH = () => {
-      return (
-          <p>test</p>
-      )
+function toDOHOutput({ name, id }) {
+    return (
+      <ul>
+        {toDOHs.map((toDOH) => (
+          <li >{toDOH.name} {toDOH.description1} {toDOH.description2}</li>
+        ))}
+      </ul>
+    )
   }
   
-  export default toDOH
+  
+  export default toDOHOutput;
