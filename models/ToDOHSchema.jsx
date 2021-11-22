6c27304e282ef3d1cd34c2abd015f3d2c6b5878c
Robert Dessin <robert.dessin@aboutsource.net>
2021-11-23 00:08:44 +0100

    remove unused dependencies

--- models/ToDOHSchema.jsx
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const toDOHSchema = {
-    title: String,
-    content: String
-}
-const createToDOH = mongoose.model("createToDOH", toDOHSchema);
-
-module.exports = createToDOH;
