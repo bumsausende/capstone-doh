3aea36d41df20d3ffface1ab9fd247ece1b0818c
Robert Dessin <robert.dessin@aboutsource.net>
2021-11-23 00:15:29 +0100

    fix background image

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -7,16 +7,13 @@ export const GlobalStyles = createGlobalStyle`
 body {
 
 background-image: url('./images/backgroundBasic.svg');
+background-repeat: no-repeat;
+background-size: cover;
 background-color: #5DC8A8;
 padding: 0;
 margin: 0;
-font-family: "Lekton";}
-@media (max-width: 768px) {
-      background-image: url("./images/backgroundBasic.svg");
-}
-* {
-  box-sizing: border-box;
-  font-family: "Lekton";
+box-sizing: border-box;
+font-family: "Lekton";
 }
 `;
 
